b8fb26cf429db4a63ecfa383f4040e46d6dd211f
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-29 06:14:10 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,6 +12,7 @@ attr_accessor :name, :roster, :grade, :student
     if @roster.has_key?(grade) && @roster.include?(student) == false
       @roster[grade] << student
     elsif @roster.has_key?(grade) == false
+      
     end
   end
 end
