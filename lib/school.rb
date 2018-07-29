5e54850a7f59b368d6775bc40e67e204a880806c
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-29 06:35:12 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -22,7 +22,7 @@ attr_accessor :name, :roster, :grade, :student
   
   def sort
     @roster.each_value do |key, value|
-      @roster[key] << value.sort
+      value.sort
     end
     @roster
   end
