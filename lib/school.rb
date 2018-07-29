bbe5feecfc7ab4833db3205acfea75628516ffa6
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-29 05:55:15 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,7 +12,7 @@ attr_accessor :name, :roster, :grade, :roster
     if @roster.has_key?(grade)
       @roster[grade] << "#{name}"
     else
-      @roster = @roster.merge!(:grade, name)
+      @roster.merge!(:grade, name)
     #   @roster[grade] << "#{name}"
     # elsif @roster.has_key?(grade) == false
     #   @roster.merge!(:grade, "#{name}")
