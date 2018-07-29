0b4dcd71a3fb031f5602dc1573cb95d37a7deef2
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-29 06:00:04 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,7 @@ attr_accessor :name, :roster, :grade, :roster
   def add_student(name, grade)
     @roster[grade] = []
     if @roster.has_key?(grade)
-      @roster[grade] << "#{name}"
+      @roster[grade] << name
     elsif @roster.has_key?(grade) == false
       @roster[grade] = "#{name}"
     end
